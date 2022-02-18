a6d9d8b220b48ddab08242f4759be065358aa18a
Damian <przpiw@gmail.com>
2022-02-19 06:34:35 +1030

    add image support

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import '../styles/index.scss'
-
+import square from '../assets/images/sample.png'
 const App = () => {
   return (
     <>
@@ -7,6 +7,7 @@ const App = () => {
         <main>
           <section>
             <h2 style={{color:'rgb(229, 226, 226)'}}>React Boilerplate</h2>
+            <img src={square} alt="square" width="100"/>
           </section>
         </main>
       </section>
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,29 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 let mode = 'development'
+let target = 'web'
 if (process.env.NODE_ENV === 'production') {
   mode = 'production'
 }
 module.exports = {
   mode: mode,
+  target: target,
+  output: {
+    assetModuleFilename: 'images/[hash][ext][query]',
+  },
   module: {
     rules: [
       {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset',
+      },
+      {
         test: /\.(s[ac]|c)ss$/i,
         use: [
-          MiniCssExtractPlugin.loader,
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { publicPath: '' },
+          },
           'css-loader',
           'postcss-loader',
           'sass-loader',
